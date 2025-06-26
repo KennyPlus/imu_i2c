0b3104fd195e1a50aa47c3896430abc5fa2f1c05
Kenny <148837522+KennyPlus@users.noreply.github.com>
2025-06-26 22:32:51 +0800

    Create bmi270_config_file.h

---
+++ driver/bmi270_config_file.h
@@ -0,0 +1,10 @@
+#ifndef BMI270_CONFIG_FILE_H
+#define BMI270_CONFIG_FILE_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+extern const uint8_t bmi270_config_file[];
+extern const size_t bmi270_config_file_len;
+
+#endif
